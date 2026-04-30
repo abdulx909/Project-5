80d50136f495b855b3f4b4c4667fed9052e14ee2
abdulx909 <abdul909@umn.edu>
2026-04-30 16:41:31 -0500

    completed todos for server

--- server/handleClients.c
+++ server/handleClients.c
@@ -4,15 +4,72 @@ pthread_mutex_t clientCountLock = PTHREAD_MUTEX_INITIALIZER, letterLock = PTHREA
 // print timestamp
 // You can get timestamp using localtime()
 void timestamp(){
+  time_t now;
+  struct tm *timeInfo;
+  char timeString[80];
+
+  time(&now);
+  timeInfo = localtime(&now);
+  strftime(timeString, sizeof(timeString), "%a %b %d %H:%M:%S %Y", timeInfo);
+
+  printf("[%s]", timeString);
+
 }
 
 // Count the occurence of all 26 letters, and update letterCount[] correspondingly
 void calculateLetterCount(char *filename){
+  FILE *fp;
+  char line[MSGLEN];
+  int localCount[ALPHACOUNT];
+
+  for (int i = 0; i < ALPHACOUNT; i++) {
+      localCount[i] = 0;
+  }
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+      return;
+  }
+
+  while (fgets(line, sizeof(line), fp) != NULL) {
+      if (line[0] == '\0' || line[0] == '\n') {
+          continue;
+      }
+
+      char first = tolower(line[0]);
+      if (first >= 'a' && first <= 'z') {
+          localCount[first - 'a']++;
+      }
+  }
+
+  fclose(fp);
+
+  pthread_mutex_lock(&letterLock);
+  for (int i = 0; i < ALPHACOUNT; i++) {
+      letterCount[i] += localCount[i];
+  }
+  pthread_mutex_unlock(&letterLock);
 }
 
 // Create a string based on letterCount[], the final return character array 
 // The string looks like count1#count2#....#count26#
 char* convertLetterCountToChar(){
+  char *result;
+  char temp[32];
+
+  result = (char *)malloc(512);
+  if (result == NULL) {
+      return NULL;
+  }
+
+  result[0] = '\0';
+
+  for (int i = 0; i < ALPHACOUNT; i++) {
+      sprintf(temp, "%d#", letterCount[i]);
+      strcat(result, temp);
+  }
+
+  return result;
 }
 
 // called by threads created on server
@@ -23,25 +80,104 @@ void* processClients(void* args){
 
   while (1) {
     struct msg_buffer buff;
+    ssize_t recvValue;
 
     // Waiting to received from client process
     // Should store the message in buff
+    timestamp();
+    printf("Waiting to rcv from client process %d\n", tdata.clientID - 1);
+
+    recvValue = msgrcv(
+      tdata.msgqueue,
+      &buff,
+      sizeof(buff.mesg_text),
+      tdata.clientID,
+      0
+    );
+
+    if (recvValue == -1) {
+      perror("msgrcv");
+      continue;
+    }
 
     // Handle the received message
     if (strcmp(buff.mesg_text, "END") == 0){
       // if the message is END:
+      int allDone = 0;
+      char *finalResult;
+      struct msg_buffer sendBuff;
+
+      timestamp();
+      printf("Thread %d received END from client process %d\n",
+             tdata.clientID - 1,
+             tdata.clientID - 1);
+
+      pthread_mutex_lock(&clientCountLock);
+      completedClients++;
+      pthread_mutex_unlock(&clientCountLock);
 
       // wait for all threads to complete
+      while (!allDone) {
+        pthread_mutex_lock(&clientCountLock);
+        if (completedClients == num_clients) {
+            allDone = 1;
+        }
+        pthread_mutex_unlock(&clientCountLock);
+
+        if (!allDone) {
+            usleep(1000);
+        }
+      }
 
       // Convert letter array to character array using convertLetterCountToChar()
       // and send it back to client
-      // After that, break the while loop to exit the server
+      finalResult = convertLetterCountToChar();
+      if (finalResult == NULL) {
+        pthread_exit(NULL);
+      }
 
+      sendBuff.mesg_type = 2000 + tdata.clientID;
+      strcpy(sendBuff.mesg_text, finalResult);
+
+      timestamp();
+      printf("Thread %d sending final letter count to client process %d\n",
+             tdata.clientID - 1,
+             tdata.clientID - 1);
+
+      if (msgsnd(tdata.msgqueue, &sendBuff, sizeof(sendBuff.mesg_text), 0) == -1) {
+        perror("msgsnd");
+      }
+
+      free(finalResult);
+      // After that, break the while loop to exit the server
+      break;
     }else{
       // if the message is not END, then it can only be a file name (one line in clienti.txt)
       // Call calculateLetterCount() to count letters
-    }    
+      struct msg_buffer ackBuff;
 
-    // After the file is read completely, send an "ACK" message back
+      timestamp();
+      printf("Thread %d received %s from client process %d\n",
+             tdata.clientID - 1,
+             buff.mesg_text,
+             tdata.clientID - 1);
+
+      calculateLetterCount(buff.mesg_text);
+      
+      // After the file is read completely, send an "ACK" message back **edited line
+      ackBuff.mesg_type = 1000 + tdata.clientID;
+      strcpy(ackBuff.mesg_text, "ACK");
+
+      timestamp();
+      printf("Thread %d sending ACK to client %d for %s\n",
+              tdata.clientID - 1,
+              tdata.clientID - 1,
+              buff.mesg_text);
+
+      if (msgsnd(tdata.msgqueue, &ackBuff, sizeof(ackBuff.mesg_text), 0) == -1) {
+        perror("msgsnd");
+      }
+    }   
   }
+  return NULL;
 }
--- server/server.c
+++ server/server.c
@@ -1,5 +1,9 @@
 #include "server.h"
 #include "handleClients.h"
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <errno.h>
 
 int letterCount[26];
 int completedClients = 0;
@@ -7,22 +11,76 @@ int num_clients; // the number of client threads
 
 
 int main(int argc, char *argv[]){
+  pthread_t threads[1000];
+  struct thd_data threadData[1000];
+  int msgqueue;
+  key_t key;
+
+  setbuf(stdout, NULL);
+
   // server only take one argument
   // Usage: ./server [process_num]
+  if (argc != NUMARGS) {
+    fprintf(stderr, "Usage: ./server [process_num]\n");
+    return 1;
+  }
+
+  num_clients = atoi(argv[1]);
+  if (num_clients <= 0 || num_clients > 1000) {
+    fprintf(stderr, "Invalid number of clients.\n");
+    return 1;
+  }
 
   // Print log for server start
+  timestamp();
+  printf("Server starts...\n");
 
-  // intialize letterCount content to zero
+  // initialize letterCount content to zero
+  for (int i = 0; i < 26; i++) {
+    letterCount[i] = 0;
+  }
 
   // get access to msg Queue using msgget()
+  key = ftok("..", 'q');
+  if (key == -1) {
+    perror("ftok");
+    return 1;
+  }
+
+  msgqueue = msgget(key, 0666 | IPC_CREAT);
+  if (msgqueue == -1) {
+    perror("msgget");
+    return 1;
+  }
 
   // create threads to handle incoming client requests
   // num_clients: the number of threads
   // pthread_create() should call processClients() (defined in handleClients.c) and the param should be struct thd_data* 
   // NOTE: clientID starts from 1
+  for (int i = 0; i < num_clients; i++) {
+    threadData[i].msgqueue = msgqueue;
+    threadData[i].clientID = i + 1;
+
+    if (pthread_create(&threads[i], NULL, processClients, &threadData[i]) != 0) {
+      perror("pthread_create");
+      msgctl(msgqueue, IPC_RMID, NULL);
+      return 1;
+    }
+  }
 
   // join all the threads
+  for (int i = 0; i < num_clients; i++) {
+    pthread_join(threads[i], NULL);
+  }
 
-  //close msgqueue and return
+  // close msgqueue and return
+  timestamp();
+  printf("Server ends...\n");
+
+  if (msgctl(msgqueue, IPC_RMID, NULL) == -1) {
+    perror("msgctl");
+    return 1;
+  }
+
   return 0;
 }
